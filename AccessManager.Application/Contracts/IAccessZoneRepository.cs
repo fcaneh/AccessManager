5c1f887730d4e0d092e93a28906062d3be3da9f5
fcanehan <fcanehan.jobs@gmail.com>
2026-05-29 10:38:15 +0200

    creation Interfaces features et userstories acces zone

---
+++ AccessManager.Application/Contracts/IAccessZoneRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AccessManager.Domain.Entities;
+
+namespace AccessManager.Application.Contracts
+{
+    public interface IAccessZoneRepository
+    {
+        IEnumerable<AccessZone> GetAllZones();
+        AccessZone? GetZoneById(Guid zoneId);
+    }
+}
---
+++ AccessManager.Application/Features/Access/CheckAccess/CheckAccessHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessManager.Application.Features.Access.CheckAccess
+{
+    internal class CheckAccessHandler
+    {
+    }
+}
