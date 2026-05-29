e0802b052370cd463976d8b3e4a948f03ec154ab
fcanehan <fcanehan.jobs@gmail.com>
2026-05-30 00:29:31 +0200

    setup console to entry point

---
+++ AccessManager.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessManager.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Access Manager Console Application started");
+        }
+    }
+}
