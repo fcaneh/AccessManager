f38d1d4c76ef6c2feb03633d189f83d9a6147649
fcanehan <fcanehan.jobs@gmail.com>
2026-06-01 18:34:37 +0200

    statistics user accessattemp

---
+++ AccessManager.Application/Features/Statistics/GetStatisticsHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AccessManager.Application.Contracts;
+using AccessManager.Domain.Enums;
+
+namespace AccessManager.Application.Features.Statistics
+{
+    public class GetStatisticsHandler
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IAccessAttemptRepository _attemptRepository;
+
+        public GetStatisticsHandler(IUserRepository userRepository, IAccessAttemptRepository attemptRepository)
+        {
+            _userRepository = userRepository;
+            _attemptRepository = attemptRepository;
+        }
+
+
+        public GetStatisticsResponse Handle(GetStatisticsQuery  query)
+        {
+            var users = _userRepository.GetAllUsers().ToList();
+            var accessAttempts = _attemptRepository.GetAllAccessAttempts().ToList();
+
+            return new GetStatisticsResponse
+            {
+                UsersCount = users.Count,
+                ActiveUsersCount = users.Count(user => user.IsActive),
+                InactiveUsersCount = users.Count(user => !user.IsActive),
+                TotalAccessAttempts = accessAttempts.Count,
+                GrantedAccessAttempts = accessAttempts.Count(accessAttempt => accessAttempt.AccessResult == AccessResult.Granted),
+                DeniedAccessAttempts = accessAttempts.Count(accessAttempt => accessAttempt.AccessResult != AccessResult.Granted),
+                MostUsedZone = accessAttempts
+                    .GroupBy(attempt => attempt.ZoneName)
+                    .OrderByDescending(attempt => attempt.Count())
+                    .FirstOrDefault()?.Key ?? "N/A"
+            };
+        }
+    }
+}
---
+++ AccessManager.Application/Features/Statistics/GetStatisticsResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AccessManager.Application.Features.Statistics
+{
+    public record GetStatisticsResponse
+    {
+        public int UsersCount { get; init; }
+        public int ActiveUsersCount { get; init; }
+        public int InactiveUsersCount { get; init; }
+        public int TotalAccessAttempts { get; init; }
+        public int GrantedAccessAttempts { get; init; }
+        public int DeniedAccessAttempts { get; init; }
+        public string MostUsedZone { get; init; } = string.Empty;
+    }
+}
